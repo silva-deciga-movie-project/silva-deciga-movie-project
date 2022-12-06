a91e4ce6d1efad73af0bf5f0bc79db7c0a1200b6
Steve <stevedee97@gmail.com>
2022-12-06 15:57:11 -0600

    COB

--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
-// //Making Request To Glitch //
-// fetch('https://highfalutin-defiant-hickory.glitch.me/movies').then((data) => {
-//     return data.json();
-// }).then((movieData) => {
-//     console.log(movieData);
-// });
+// Fetch to glitch, the data coming back is SUPPOSED to populate cards. But guess what bro, it doesn't. HELP. //
 
-// Search //
+let html =""
+fetch('https://highfalutin-defiant-hickory.glitch.me/movies').then((data) => {
+    return data.json();
+}).then((data) => {
+    for(let movie of data) {
+
+        $(".movie-list-container").append(
+            `<div class="movie-list-wrapper">
+    <div class="movie-list">
+        <div class="movie-list-item">
+            <img class="movie-list-item-img" src="${data.Poster}" alt=""/>
+            <span class="movie-list-item-title">${data.Title}</span>
+            <p class="movie-list-item-desc">${data.Plot}</p>
+            <button class="movie-list-itmem-button"></button>
+            </div>
+        </div>
+    </div>
+</div>`
+        )}
+    console.log(data);
+
+});
+
+// Search button is clicked, then data is extracted from the searchbox, sends the data through fetch to the API. Once the API receieves the data it returns in an object and THEN "CreateCard" function is called to populate the cards. //
    $("#search-button").click((e) => {
         e.preventDefault();
         let userInput = $("#searchBox").val()
@@ -20,7 +38,7 @@
 
     })
 
-// Adding Movies to DataBase //
+// Adding Movies to Glitch DataBase //
 $("#addButton").on("click", function (e) {
     e.preventDefault();
     const submitObj = {
@@ -47,7 +65,7 @@ $("#addButton").on("click", function (e) {
 })
 
 
-//Delete Functionality //
+//Delete movies from glitch database Functionality //
 
 $(".deleteButton").click(function (e) {
     console.log($("#movieDelete").val());
